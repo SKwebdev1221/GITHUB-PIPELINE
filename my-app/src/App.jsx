2f069597c0ee7e7fa732f2c388a8235670185c14
SKwebdev1221 <sahilvaibhav1221@gmail.com>
2026-08-15 20:41:02 +0530

    Changed App.jsx

--- my-app/src/App.jsx
+++ my-app/src/App.jsx
@@ -16,7 +16,7 @@ function App() {
           <img src={viteLogo} className="vite" alt="Vite logo" />
         </div>
         <div>
-          <h1>Vite + React - Webhook Test</h1>
+          <h1>Vite + React - Webhook Test 2</h1>
           <p>
             Edit <code>src/App.jsx</code> and save to test <code>HMR</code>
           </p>
